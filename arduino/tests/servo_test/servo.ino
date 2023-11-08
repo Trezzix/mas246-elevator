863b2c98c0d0abf7f49e9e49a57c51c22817b2af
creamytoad <mathias.lervik@hotmail.com>
2023-11-08 15:58:31 +0100

    PID without optimizations in servotest

--- arduino/tests/servo_test/servo.ino
+++ arduino/tests/servo_test/servo.ino
@@ -1,3 +1,19 @@
+namespace PIDvars //all float since used for calculation
+{
+  float error = 0;
+  float errorDot = 0;
+  float errorInt = 0;
+  float errorPrev = 0;
+
+  float kp = 1.5;
+  float ki = 0;
+  float kd = 1;
+
+  float u = 0;
+  float uDot = 0;
+}
+
+using namespace PIDvars;
 using namespace servoVars;
 
 void servoInit()
@@ -5,44 +21,113 @@ void servoInit()
   pinMode(enable,OUTPUT);
   pinMode(phase,OUTPUT);
   pinMode(decay,OUTPUT);
-  digitalWrite(decay,LOW); // sets to slow in decay mode
+  digitalWrite(decay,LOW); // sets to slow in decay mode, maybe switch to bipolar for simpler PID use?
+  analogWrite(enable,0);
+
+  motorSpeedMax = static_cast<int>((elevatorSpeed/maxRPS)*255.0); //174
 }
 
-void moveElevator(motorState dir,int floorReq) //move to servo dingus
+void moveElevator(motorState dir,int floorReq) //tell servo what to do, dir useless?
 {
   //protection so not going up when floor is below or vice-versa? does it need to know the floor?
-
-  if (dir == down)
+  float output = 0.0;
+  unsigned long tx = 0;
+  unsigned long tx_prev = 0;
+  float dt = 0.0;
+  unsigned long timeAccStart;
+  float startHeight = currentHeight;
+  
+  if (true)//check if doors are closed with sensor
   {
-    //accelerate:
-    
-    //constant speed
+    switch (dir)
+    {
+      case up:
+      {
+        //Serial.println("moving");
+        timeAccStart = millis();
+        floorReq = floorReq * floorDist;
 
-    //decelerate
+        while(true) //control with PID of position to :
+        {
+          sensorVars::heightMoved = ((static_cast<float>( readServoPosition() ) / sensorVars::cps) * meterPerRot); //10 rotations for 5m moved  
+          currentHeight = sensorVars::heightMoved + startHeight;         
+          tx = (static_cast<float>(millis() - timeAccStart)); //time between start and now
+          dt = static_cast<float>(tx - tx_prev)/1000.0;//converted to s, float because used in calcs
 
-    //get to correct position with sensor
-    
+          //Serial.print("currentHeight:");
+          //Serial.print(currentHeight);
+          //Serial.print(",");
+
+          //Serial.print("height_moved:");
+          //Serial.print(sensorVars::heightMoved);
+          //Serial.print(",");
+          
+          error = (static_cast<float>(floorReq)) - currentHeight; //converted to m 
+          errorDot = (error - errorPrev)/dt;
+          errorInt += (error-errorPrev) * dt;
+
+          Serial.print("error:");
+          Serial.print(error);
+          Serial.print(",");
+
+          u = (kp * error) + (ki * errorInt) + (kd * errorDot);
+          
+          Serial.print("PID_output:");
+          Serial.print(u);
+          Serial.print(",");
+          
+          output = writeServo(u,output,dt); //input needs to be a derivative of position
+          
+          Serial.print("current_speed:");
+          Serial.println(output);
+          errorPrev = error;
+          tx_prev = tx;
+          //check if new floorReq?
+        }
+        //Serial.println("move completed");
+      }
+      case down:
+      {
+        
+      }
+    }
   }
   else
   {
-    //same as above, opposite direction
+    //error: doors not closed
   }
 }
 
-void writeServo(motorState dir,int motorSpeed,bool disable)
-{
-    unsigned long timeAccStart = millis(); //start of acceleration
-    digitalWrite(phase, LOW); //why?
-    analogWrite(enable,motorSpeed); //enable motor
-    delay(2000); //replace with us time or milis
+float writeServo(float motorSpeedPerc, float motorPrev, float dt) //input: -1 to 1,, should include a stop function?
+{  
+  bool servoDir = false;
+  int motorSpeedPWM =0;
+  //float maxAccPWM = (elevatorAcc/meterPerRot));
+  if (motorSpeedPerc >= 1) // capping to max speed
+  {
+    motorSpeedPerc = 1.0;
+  }
+  else if (motorSpeedPerc <= -1)
+  {
+    motorSpeedPerc = -1.0;
+  }
 
-    
-    if (disable)
-    {
-      analogWrite(enable,0); //stop motor
-    }
-    else
-    {
-      //do nothing, for smooth acceleration
-    }
+  servoDir = (motorSpeedPerc >= 0); //true if positive, false if negative
+  digitalWrite(phase,!servoDir);
+  
+  motorSpeedPWM = static_cast<int>(abs(motorSpeedPerc) * motorSpeedMax);
+  
+  //if (speedDot >= )
+  
+  Serial.print("motorSpeedPerc");
+  Serial.print(motorSpeedPerc);
+  Serial.print(",");
+
+  speedDot = ((((motorSpeedPerc - motorPrev)*static_cast<float>(motorSpeedMax))/(dt)) / 255.0)*maxRPS;
+  Serial.print("speedDot:");
+  Serial.print(speedDot); //unit?
+  Serial.print(",");
+  
+  analogWrite(enable,motorSpeedPWM);
+  return motorSpeedPerc;
 }
--- arduino/tests/servo_test/servo_test.ino
+++ arduino/tests/servo_test/servo_test.ino
@@ -12,25 +12,71 @@ enum motorState
     idleMotor
   };
 
+enum moveType
+  {
+    accelerate,
+    constMove,
+    decelerate,
+    PIDcontrol,
+    stopMove //temporary
+  };
+
+namespace sensorVars
+{
+  const byte encoderPinA = 20;
+  const byte encoderPinB = 21;
+  const float cps(32.0 * 131.0); //float since only used for maths, cps * gear ratio of 131:1
+  volatile long encoderPos = 0;
+  volatile boolean A_set = true;
+  volatile boolean B_set = true;
+  float heightMoved = 0.0;
+}
+
 namespace servoVars
 {
-  int motorspeed = 64; //max 255
-  int enable = 7;
-  int phase = 6;
-  int decay = 5;
+  int motorSpeedMax = 0; //max 255
+  const int enable = 7;
+  const int phase = 6;
+  const int decay = 5; // should be defined?
   float elevatorAcc = 1.2; //m/s^2
   float elevatorSpeed = 1.0; //m/s
+  float floorDist = 5.0; //m
+  float meterPerRot = 1; //m/rotation
+  float maxRPS = (11500.0/131.0)/60.0; //rps of weight at 255 PWM, 
+  float speedDot = 0; 
 }
 
 //declarations
 void servoInit();
+void servoEncoderInit();
+float writeServo(float motorSpeedPerc, int motorPrev, unsigned long dt);
+
+//global vars
+int currentFloor; //for visuals
+float currentHeight;
+bool a = true;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   servoInit();
+  servoEncoderInit();
+  currentFloor = 3;
+  currentHeight = currentFloor * servoVars::floorDist;
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  if (a)
+  {
+    moveElevator(up,1);
+    delay(500);
+    analogWrite(servoVars::enable,0);
+    
+    float temp = static_cast<float>( readServoPosition() ) / sensorVars::cps;
+    //Serial.print(" servo pos: ");
+    //Serial.println( temp );
+    a = false; //run once
+  }
 }
